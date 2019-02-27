b81d3b26f5c586234cf4acff9594df28b54332ff
Hernan Garchtrom <hernan.garchtrom@possible.com>
2019-02-27 12:36:37 -0300

    Fixed indentation

--- src/components/FilmList/FilmList.js
+++ src/components/FilmList/FilmList.js
@@ -2,22 +2,21 @@ import React from 'react';
 import { FilmConsumer } from '../../contexts/FilmContext';
 import FilmListItem from '../FilmListItem/FilmListItem';
 
-
 class FilmList extends React.Component {
-    renderFilms(films) {
-        const elements = films
-            .sort((a, b) => a.episode_id - b.episode_id)
-            .map(film => <FilmListItem key={film.episode_id} {...film} />);
-        return elements;
-    }
-
-    render() {
-        return (
-            <FilmConsumer>
-                { this.renderFilms }
-            </FilmConsumer>            
-        );
-    }
+  renderFilms(films) {
+    const elements = films
+    .sort((a, b) => a.episode_id - b.episode_id)
+    .map(film => <FilmListItem key={film.episode_id} {...film} />);
+    return elements;
+  }
+  
+  render() {
+  return (
+    <FilmConsumer>
+      { this.renderFilms }
+    </FilmConsumer>            
+    );
+  }
 }
-
+  
 export default FilmList;
--- src/contexts/FilmContext.js
+++ src/contexts/FilmContext.js
@@ -4,31 +4,31 @@ const FilmContext = React.createContext('films');
 
 
 class FilmProvider extends React.Component {
-    state = {
-        films: []
+  state = {
+    films: []
+  }
+  
+  async componentDidMount() {
+    const response = await fetch('https://swapi.co/api/films/');
+    const json = await response.json();
+    
+    this.setState({
+      films: [ ...json.results ]
+    });
+  }
+  
+  render() {
+    return (
+      <FilmContext.Provider value={this.state.films}>
+      { this.props.children }
+      </FilmContext.Provider>
+      )
     }
-
-    async componentDidMount() {
-        const response = await fetch('https://swapi.co/api/films/');
-        const json = await response.json();
-        
-        this.setState({
-            films: [ ...json.results ]
-        });
-    }
-
-    render() {
-        return (
-            <FilmContext.Provider value={this.state.films}>
-                { this.props.children }
-            </FilmContext.Provider>
-        )
-    }
-}
-
-const FilmConsumer = FilmContext.Consumer;
-
-export {
+  }
+  
+  const FilmConsumer = FilmContext.Consumer;
+  
+  export {
     FilmProvider,
     FilmConsumer
-};
+  };
